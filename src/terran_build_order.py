0194d77e8103257ab6677a8d142716182cf8d36b
Nicolas Dupont <nicolas@akeneo.com>
2018-02-18 19:01:38 +0100

    extract orders from build order

--- src/terran_build_order.py
+++ src/terran_build_order.py
@@ -29,19 +29,79 @@ _QUEUED = [1]
 
 # cf http://liquipedia.net/starcraft2/MMM_Timing_Push
 class MMMTimingPushBuildOrder:
+
+    orders = None
+    current_order = None
+    current_order_index = None
+
+    def __init__(self, base_top_left):
+        base_location = BaseLocation(base_top_left)
+        self.orders = [
+            BuildSupplyDepot(base_location),
+            BuildBarracks(base_location),
+            TrainMarine(base_location),
+            NoOrder(base_location)
+        ]
+        self.current_order_index = 0
+        self.current_order = self.orders[0]
+
+    def action(self, obs):
+
+        if self.current_order.done():
+            self.next_order()
+
+        return self.current_order.execute(obs)
+
+    def next_order(self):
+        self.current_order_index = self.current_order_index + 1
+        if self.current_order_index < len(self.orders):
+            self.current_order = self.orders[self.current_order_index]
+            print(self.current_order)
+        else:
+            self.current_order = self.orders[len(self.orders) - 1]
+
+class BaseLocation:
+
     base_top_left = None
-    supply_depot_built = False
-    scv_selected = False
-    barracks_built = False
-    barracks_selected = False
-    barracks_rallied = False
-    army_selected = False
-    army_rallied = False
 
     def __init__(self, base_top_left):
         self.base_top_left = base_top_left
 
-    def action(self, obs):
+    def top_left(self):
+        return self.base_top_left
+
+    def transform_location(self, x, x_distance, y, y_distance):
+        if not self.base_top_left:
+            return [x - x_distance, y - y_distance]
+
+        return [x + x_distance, y + y_distance]
+
+
+class Order:
+    base_location = None
+
+    def __init__(self, base_location):
+        self.base_location = base_location
+
+    def done(self):
+        raise NotImplementedError( "Should be implemented by concrete order" )
+
+    def execute(self, obs):
+        raise NotImplementedError( "Should be implemented by concrete order" )
+
+
+class BuildSupplyDepot(Order):
+
+    scv_selected = False
+    supply_depot_built = False
+
+    def __init__(self, base_location):
+        Order.__init__(self, base_location)
+
+    def done(self):
+        return self.supply_depot_built
+
+    def execute(self, obs):
         if not self.supply_depot_built:
             if not self.scv_selected:
                 unit_type = obs.observation["screen"][_UNIT_TYPE]
@@ -56,18 +116,33 @@ class MMMTimingPushBuildOrder:
                 unit_type = obs.observation["screen"][_UNIT_TYPE]
                 unit_y, unit_x = (unit_type == _TERRAN_COMMANDCENTER).nonzero()
 
-                target = self.transform_location(int(unit_x.mean()), 0, int(unit_y.mean()), 20)
+                target = self.base_location.transform_location(int(unit_x.mean()), 0, int(unit_y.mean()), 20)
 
                 self.supply_depot_built = True
 
                 return actions.FunctionCall(_BUILD_SUPPLYDEPOT, [_NOT_QUEUED, target])
+        return actions.FunctionCall(_NOOP, [])
 
-        elif not self.barracks_built:
+class BuildBarracks(Order):
+
+    barracks_built = False
+    barracks_selected = False
+    barracks_rallied = False
+    # TODO select vcs
+
+    def __init__(self, base_location):
+        Order.__init__(self, base_location)
+
+    def done(self):
+        return self.barracks_rallied
+
+    def execute(self, obs):
+        if not self.barracks_built:
             if _BUILD_BARRACKS in obs.observation["available_actions"]:
                 unit_type = obs.observation["screen"][_UNIT_TYPE]
                 unit_y, unit_x = (unit_type == _TERRAN_COMMANDCENTER).nonzero()
 
-                target = self.transform_location(int(unit_x.mean()), 20, int(unit_y.mean()), 0)
+                target = self.base_location.transform_location(int(unit_x.mean()), 20, int(unit_y.mean()), 0)
 
                 self.barracks_built = True
 
@@ -86,11 +161,27 @@ class MMMTimingPushBuildOrder:
             else:
                 self.barracks_rallied = True
 
-                if self.base_top_left:
+                if self.base_location.top_left():
                     return actions.FunctionCall(_RALLY_UNITS_MINIMAP, [_NOT_QUEUED, [29, 21]])
 
                 return actions.FunctionCall(_RALLY_UNITS_MINIMAP, [_NOT_QUEUED, [29, 46]])
-        elif obs.observation["player"][_SUPPLY_USED] < obs.observation["player"][_SUPPLY_MAX] and _TRAIN_MARINE in obs.observation["available_actions"]:
+        return actions.FunctionCall(_NOOP, [])
+
+
+class TrainMarine(Order):
+
+    barracks_selected = False
+    army_selected = False
+    army_rallied = False
+
+    def __init__(self, base_location):
+        Order.__init__(self, base_location)
+
+    def done(self):
+        return self.army_rallied
+
+    def execute(self, obs):
+        if obs.observation["player"][_SUPPLY_USED] < obs.observation["player"][_SUPPLY_MAX] and _TRAIN_MARINE in obs.observation["available_actions"]:
             return actions.FunctionCall(_TRAIN_MARINE, [_QUEUED])
         elif not self.army_rallied:
             if not self.army_selected:
@@ -103,15 +194,20 @@ class MMMTimingPushBuildOrder:
                 self.army_rallied = True
                 self.army_selected = False
 
-                if self.base_top_left:
+                if self.base_location.top_left():
                     return actions.FunctionCall(_ATTACK_MINIMAP, [_NOT_QUEUED, [39, 45]])
 
                 return actions.FunctionCall(_ATTACK_MINIMAP, [_NOT_QUEUED, [21, 24]])
-
         return actions.FunctionCall(_NOOP, [])
 
-    def transform_location(self, x, x_distance, y, y_distance):
-        if not self.base_top_left:
-            return [x - x_distance, y - y_distance]
 
-        return [x + x_distance, y + y_distance]
+class NoOrder(Order):
+
+    def __init__(self, base_location):
+        Order.__init__(self, base_location)
+
+    def done(self):
+        return True
+
+    def execute(self, obs):
+        return actions.FunctionCall(_NOOP, [])
