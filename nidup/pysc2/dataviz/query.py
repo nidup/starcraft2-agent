210833aeba2174cfd4411c970ea3f475191d50b4
Nicolas Dupont <nicolas@akeneo.com>
2018-03-04 14:10:48 +0100

    add a query results command to manipulate game results

---
+++ nidup/pysc2/dataviz/query.py
@@ -0,0 +1,22 @@
+
+from nidup.pysc2.learning.game_results import GameResultsTable
+
+
+class GameResultQueryBuilder:
+
+    def query(self, agent_name: str, filter: str) -> str:
+        games = GameResultsTable(agent_name)
+        available_filters = ['win', 'loss', 'draw']
+        if not filter in available_filters:
+            raise Exception('The filter "'+filter+'" is unknown, available filters are '+', '.join(available_filters))
+
+        # http://pandas.pydata.org/pandas-docs/stable/dsintro.html
+        total_games = 0
+        total_filtered_games = 0
+        for index in range(len(games.table)):
+            total_games = total_games + 1
+            if games.table.iloc[index][filter] == 1:
+                total_filtered_games = total_filtered_games + 1
+                print("\n"+str(games.table.iloc[index]))
+
+        print("\nTotal filtered "+str(total_filtered_games) + " (on "+str(total_games)+")")
